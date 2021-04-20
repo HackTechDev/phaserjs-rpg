8a20395c736e1402d714f89a18d514f8e6c99c76
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2021-04-20 23:34:37 +0200

    Update size character image

--- game/Level.js
+++ game/Level.js
@@ -15,9 +15,9 @@ class Level extends Phaser.Scene {
         // Player sprite.
         this.load.spritesheet({
             key: 'player',
-            url: "game/assets/playera.png",
-            frameConfig: {frameWidth: 48,  //The width of the frame in pixels.
-                          frameHeight: 64, //The height of the frame in pixels. Uses the frameWidth value if not provided.
+            url: "game/assets/player.png",
+            frameConfig: {frameWidth: 24,  //The width of the frame in pixels.
+                          frameHeight: 32, //The height of the frame in pixels. Uses the frameWidth value if not provided.
                           startFrame: 0,   //The first frame to start parsing from.
                           endFrame: 12,    //The frame to stop parsing at. If not provided it will calculate the value based on the image and frame dimensions.
                           margin: 0,       //The margin in the image. This is the space around the edge of the frames.
